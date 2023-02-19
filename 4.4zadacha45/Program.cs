150e3560bf4bed68ff492411ae7b8591c0746c74
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-20 00:01:23 +0300

    45 zadacha

---
+++ 4.4zadacha45/Program.cs
@@ -0,0 +1,30 @@
+﻿// *Задача 45: 
+// Напишите программу, которая будет создавать копию заданного массива 
+//  с помощью поэлементного копирования.
+
+int[] b = GetArray(10, 0, 40);
+Console.WriteLine(String.Join(" ", b));
+int[] s = CopyMass(b);
+Console.WriteLine(String.Join(" ", s));
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+  int[] ds = new int [size];
+  for(int i = 0; i < size; i++)
+  {
+    ds[i] = new Random().Next(minValue, maxValue);
+
+  }
+  return ds;
+}
+
+int[] CopyMass(int[] arra)
+{
+  int[] result = new int[arra.Length];
+  for(int i = 0; i<arra.Length; i++)
+  {
+    result[i] = arra[i];
+  }
+  return result;
+}
