73fa4f8d4d23d83841591a07e571ae4ae4b015bc
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-19 23:59:10 +0300

    задача про треугольник

---
+++ 2.2 treugolnik/Program.cs
@@ -0,0 +1,24 @@
+﻿// задача 40 
+// напишите программу, которая принимает на вход три числа и 
+// проверяет, может ли существовать треугольник со сторонами такой длины
+
+Console.Clear();
+Console.WriteLine("введите стороны треугольника через пробел: ");
+string[] st = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int a = int.Parse(st[0]), b = int.Parse(st[1]),  c = int.Parse(st[2]);
+
+if (IsTriange(a, b, c))
+{
+  System.Console.WriteLine("Да");
+}
+else
+{
+  System.Console.WriteLine("Нет");
+}
+
+bool IsTriange(int a, int b, int c)
+{
+  return (((a + b) > c) && ((a + c) > b) && ((c + b) > a));
+}
+
