45f703a63d37d32719b75de48f0cffd6c2c0c4d6
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-20 00:00:09 +0300

    задача сделана

---
+++ 3.3/Program.cs
@@ -0,0 +1,31 @@
+﻿// **Задача 42:**Напишите программу,
+//  которая будет преобразовывать десятичное число в двоичное.
+
+// 45 -> 101101
+
+// 3 -> 11
+
+// 2 -> 10
+Console.Clear();
+System.Console.WriteLine("Введите число: ");
+
+int n = int.Parse(Console.ReadLine());
+string res = DecToNam(n, 2);
+System.Console.WriteLine($"{n} -> {res}");
+
+// универсальный математический для перевода из 10 в любую
+
+string DecToNam(int decNamber, int otherSystem)
+{
+  string res = "";
+  string nums = "0123456789ABCDEF";
+  while(decNamber > 0)
+  {
+    int ost = decNamber / otherSystem;
+    res = nums[decNamber - otherSystem * ost] + res;
+    decNamber/=otherSystem;
+  }
+  return res;
+}
+
+// Console.WriteLine($"{Preobraz(n)}");
